ac2a19c04138de5378857d139e901657713dc642
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2022-08-04 12:31:43 +0500

    chaanges

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ const subtractDate = rightNow
   .subtract({ days: 2, months: 5, years: 12 })
   .toString();
 console.log("subtractDate", subtractDate);
+const subtractDate = rightNow
+  .subtract({ days: 2, months: 5, years: 12 })
+  .toString();
+console.log("subtractDate", subtractDate);
+const subtractDate = rightNow
+  .subtract({ days: 2, months: 5, years: 12 })
+  .toString();
+console.log("subtractDate", subtractDate);
+const subtractDate = rightNow
+  .subtract({ days: 2, months: 5, years: 12 })
+  .toString();
+console.log("subtractDate", subtractDate);
+const subtractDate = rightNow
+  .subtract({ days: 2, months: 5, years: 12 })
+  .toString();
+console.log("subtractDate", subtractDate);
+const subtractDate = rightNow
+  .subtract({ days: 2, months: 5, years: 12 })
+  .toString();
+console.log("subtractDate", subtractDate);
+const subtractDate = rightNow
+  .subtract({ days: 2, months: 5, years: 12 })
+  .toString();
+console.log("subtractDate", subtractDate);
