656a9fa498a6a25cdb268a6bed4fb1c7ec590411
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2022-08-24 00:07:49 +0500

    changes

--- script.js
+++ script.js
@@ -20,3 +20,4 @@ const subtractDate = rightNow
   .subtract({ days: 2, months: 5, years: 12 })
   .toString();
 console.log("subtractDate", subtractDate);
+console.log("subtractDate", subtractDate);
