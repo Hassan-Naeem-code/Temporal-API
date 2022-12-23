f0693e24bfa53b887887d019a487aabd849bbeab
HASSAN <hassan@HASSANs-MacBook-Air.local>
2022-12-23 10:27:10 +0500

    changes

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ const subtractDate = rightNow
   .subtract({ days: 2, months: 5, years: 12 })
   .toString();
 console.log("subtractDate", subtractDate);
+console.log("addDate", addDate);
+// Subtracting Date to Current Date
+const subtractDate = rightNow
+  .subtract({ days: 2, months: 5, years: 12 })
+  .toString();
+console.log("subtractDate", subtractDate);
+console.log("addDate", addDate);
+// Subtracting Date to Current Date
+const subtractDate = rightNow
+  .subtract({ days: 2, months: 5, years: 12 })
+  .toString();
+console.log("subtractDate", subtractDate);
+console.log("addDate", addDate);
+// Subtracting Date to Current Date
+const subtractDate = rightNow
+  .subtract({ days: 2, months: 5, years: 12 })
+  .toString();
+console.log("subtractDate", subtractDate);
